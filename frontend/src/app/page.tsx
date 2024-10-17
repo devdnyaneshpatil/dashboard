3c07b3051711a4a2a418bce531811e73e3046641
devdnyaneshpatil <dnyaneshwarpatil830831@gmail.com>
2024-10-17 10:39:44 +0530

    changed eslint config

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  const handleNavigation = (path:any) => {
+  const handleNavigation = (path:string) => {
     router.push(path);
   };
 
